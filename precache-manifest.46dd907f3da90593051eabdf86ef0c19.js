e4bac9fc106da93a51bc205265ac317e7b23e9a3
Gustav <gustav.bodin1@gmail.com>
2019-12-12 13:10:09 -0800

    Updates

---
+++ precache-manifest.46dd907f3da90593051eabdf86ef0c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d88892bf0065b9a2a609993a575a597",
+    "url": "/499_final/alpha/index.html"
+  },
+  {
+    "revision": "25bb97117f433e8c68b7",
+    "url": "/499_final/alpha/static/css/main.51bc0bfc.chunk.css"
+  },
+  {
+    "revision": "50ed106780f6ff261ebf",
+    "url": "/499_final/alpha/static/js/2.2bc869ac.chunk.js"
+  },
+  {
+    "revision": "bc4ea76a8ec636f1aa592b58bb5d318f",
+    "url": "/499_final/alpha/static/js/2.2bc869ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25bb97117f433e8c68b7",
+    "url": "/499_final/alpha/static/js/main.9d201e02.chunk.js"
+  },
+  {
+    "revision": "123236b8545e7a9b96f0",
+    "url": "/499_final/alpha/static/js/runtime-main.e023ae1e.js"
+  }
+]);
